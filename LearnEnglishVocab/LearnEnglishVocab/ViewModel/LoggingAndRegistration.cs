ac89769ab4ae9d3d760d2d3a4e875a759173eab6
Hiumpik-err <kamil.jendrusz@gmail.com>
2026-04-04 19:56:36 +0200

    adding new staff

--- LearnEnglishVocab/LearnEnglishVocab/ViewModel/LoggingAndRegistration.cs
+++ LearnEnglishVocab/LearnEnglishVocab/ViewModel/LoggingAndRegistration.cs
@@ -4,12 +4,17 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using LearnEnglishVocab.Model;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace LearnEnglishVocab.ViewModel
 {
     [ObservableObject]
     public partial class LoggingAndRegistration
     {
+        private static readonly HttpClient _httpClient = new HttpClient();
+
+
         [ObservableProperty]
         private string emaill;
         [ObservableProperty]
@@ -38,19 +43,34 @@ namespace LearnEnglishVocab.ViewModel
         public async void GoToRegistration() => await Shell.Current.GoToAsync("/registration");
         public async void GoToLoginPage() => await Shell.Current.GoToAsync("/logining");
 
-        
+
         [RelayCommand]
         public async void Register()
         {
-            User newUser = new User(Emaill, Password);
-            newUser.FirstName = Firstname ?? string.Empty;
-            newUser.LastName = Lastname ?? string.Empty;
-            newUser.Age = Age;
-            newUser.Username = Username ?? string.Empty;
+            try
+            {
+                if (Emaill is null || Password is null) throw new Exception("Essential data is empty");
+                User newUser = new User(Emaill, Password);
+                newUser.FirstName = Firstname ?? string.Empty;
+                newUser.LastName = Lastname ?? string.Empty;
+                newUser.Age = Age;
+                newUser.Username = Username ?? string.Empty;
+
+                string json = JsonSerializer.Serialize(newUser);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+                string path = "http://localhost:8000/api/register";
+                HttpResponseMessage response = await _httpClient.PostAsync(path, content);
+
+                if (response.IsSuccessStatusCode) await Shell.Current.GoToAsync("/home");
+                else throw new Exception("Invalid data");
+            }
+            catch (Exception ex)
+            {
+                await Shell.Current.DisplayAlertAsync("Warning", ex.Message, "OK");
+            }
 
 
-
-            await Shell.Current.GoToAsync("/home");
         }
 
 
